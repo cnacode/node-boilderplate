010c445f6e8b559c353a7ddbe525826ba326cf84
Sina Montazeri <sinamontazeri@outlook.com>
2020-04-05 21:19:41 +0430

    update types

--- global.d.ts
+++ global.d.ts
@@ -1,18 +1,32 @@
 //import types
-import { 
-    Request as ExpressRequest, 
+import {
+    Request as ExpressRequest,
     Response as ExpressResponse,
     NextFunction as ExpressNext,
     Application as ExpressApplication,
-} from "express";
+    RequestHandler,
+} from 'express'
+import { Logger } from 'winston'
 
 declare global {
     type EnvObject = {
-        NETWORK_SERVER_PORT: 'NETWORK_SERVER_PORT';
-    };
+        NETWORK_SERVER_PORT: 'NETWORK_SERVER_PORT'
+    }
+    type Application = ExpressApplication
+    type APIRequest = ExpressRequest
+    type APIResponse = ExpressResponse
+    type APINext = ExpressNext
 
-    type Application = ExpressApplication;
-    type APIRequest = ExpressRequest;
-    type APIResponse = ExpressResponse;
-    type APINext = ExpressNext;
+    type AsyncHandler = (fn: any) => any
+
+    type AppLogger = Logger | Console
+
+    type ServiceDependencies = {
+        asyncHandler: AsyncHandler
+        logger: AppLogger
+    }
+
+    type MethodDependencies = {
+        logger: AppLogger
+    }
 }
